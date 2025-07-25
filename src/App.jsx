bf0561506d7e84a193729e5276723f8df7f29957
Gaurav <vishwagaurav27@gmail.com>
2025-07-25 15:40:01 +0530

    Updated useEffect Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() { 
     
@@ -11,27 +11,39 @@ function App() {
   
 }
 
-
+// mounting , re-render, un-mounting
 function Counter(){
 
   const[count, setCount] = useState(0)
-
-  function increaseCount (){
+ 
+  // protect our setInterval from re-render  
+  useEffect(function(){
+    setInterval(function(){
+    setCount(count => count+1) // or 
+    /* setCount(function(count){
+      return count + 1;
+    }) */
+  },1000)
+},[])
+  
+ /* function increaseCount (){
   setCount(count + 1)
   }
+ 
+
 function dcreaseCount (){
   setCount(count - 1)
   }
   function resetCount (){
-  setCount( 0)
-  }
+  setCount(0)
+  }     */
 
 
   return <div>
     <h1 id="text">{count}</h1>
-    <button onClick ={increaseCount}>Increase Count</button>
+   {/*  <button onClick ={increaseCount}>Increase Count</button>
     <button onClick ={dcreaseCount}>Decrease Count</button>
-    <button onClick ={resetCount}>Reset Count</button>
+    <button onClick ={resetCount}>Reset Count</button>  */}
   </div>
 }
 
